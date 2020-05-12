5fd3aaa64767056d45b3bd5da6fd973932e58fe7
SufyanTariq123 <SufyanTariq056@gmail.com>
2020-05-13 02:25:14 +0500

    Updates

---
+++ precache-manifest.115dba6883d58a4e66a035cf46533d2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a44f8cddc593000e8510009c2c54fd",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "65a82551890c44f498bb",
+    "url": "/COVID-19-Tracker/static/css/main.20782bae.chunk.css"
+  },
+  {
+    "revision": "e42448a6a409e1d40bd8",
+    "url": "/COVID-19-Tracker/static/js/2.11e005ad.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Tracker/static/js/2.11e005ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a82551890c44f498bb",
+    "url": "/COVID-19-Tracker/static/js/main.af78ceb5.chunk.js"
+  },
+  {
+    "revision": "a07598c772dcf9416df7",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.be7528e3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
